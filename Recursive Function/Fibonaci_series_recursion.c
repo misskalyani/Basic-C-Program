24c687ff578302852ccc9a7ff2f80ad9e118d3e6
Kalyani <138003080+misskalyani@users.noreply.github.com>
2024-01-06 03:02:32 -0800

    Add files via upload

---
+++ Recursive Function/Fibonaci_series_recursion.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void series(int f,int s,int n)
+{
+	int t;
+	if(n>0)
+	{
+		t=f+s;
+		printf("%d\t",t);
+		f=s;
+		s=t;
+		series(f,s,n-1);
+	}
+		
+}
+void main()
+{
+	printf("01");
+	series(0,1,10);
+}
