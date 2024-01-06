c6b314ef3d0a6104c6b118478571ed8bb824020c
Kalyani <138003080+misskalyani@users.noreply.github.com>
2024-01-06 03:01:49 -0800

    Add files via upload

---
+++ Recursive Function/Fibonaci_series.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int fib(int n)
+{
+	if(n==0)
+	  return 0;
+    else if(n==1)
+    	return 1;
+    else	
+	   return(fib(n-1)+fib(n-2));  
+}
+int main()
+{
+	int n,i;
+	printf("Enter Limit::");
+	scanf("%d",&n);
+	printf("\nFibbonaci Series::");
+	for(i=0;i<=n;i++)
+	{
+		printf("\t%d",fib(i));
+	}
+}
