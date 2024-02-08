9267030dddcb0a05626ea9ef5a7087a0a4ae238a
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:39:26 +0530

    Add files via upload

---
+++ Pattern.c/Square/Lowercase_Alphabate_square.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int r,i,j;
+	printf("Enter number of rows : ");
+	scanf("%d",&r);
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=r;j++)
+		{
+		   printf("%c  ",(char)(j+96));
+		}
+		printf("\n");
+	}
+}
