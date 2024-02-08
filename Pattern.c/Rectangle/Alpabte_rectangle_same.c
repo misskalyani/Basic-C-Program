f610e32ce49cb67192fa7c702710c23df14816c4
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-08 15:43:26 +0530

    Add files via upload

---
+++ Pattern.c/Rectangle/Alpabte_rectangle_same.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int r,c,i,j;
+	printf("Enter number of rows : ");
+	scanf("%d",&r);
+	printf("Enter number of columns : ");
+	scanf("%d",&c);
+	for(i=1;i<=r;i++)
+	{
+		for(j=1;j<=c;j++)
+		{
+		   printf("%c  ",(char)(i+64));
+		}
+		printf("\n");
+	}
+}
