628d3da0713c48fc3f7f9c46e68fb93b2e0fba1e
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-11 16:46:05 +0530

    Add files via upload

---
+++ Pattern.c/Square/Uppercase_same_num_combine.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter Limit : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i%2==0)
+			{ 
+				printf("%d ",i);
+			}
+			else
+			{
+				printf("%c ",(char)(i+64));
+			}
+		}
+		printf("\n");
+	}
+}
