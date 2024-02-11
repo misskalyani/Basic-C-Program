260c4a621f600515e4d1f46b8dfe39225b1d3ed0
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-11 12:58:16 +0530

    Add files via upload

---
+++ Pattern.c/Triangle/Same_alphabate_trangle_Combine.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter Limit : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=i;j++)
+		{
+			if(i%2==0)
+			printf("%c\t",(char)(i+64));
+			else
+			printf("%d\t",i);
+		}
+		printf("\n");
+	}
+}
