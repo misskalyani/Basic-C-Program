6f02d848c9e75de4550b629f33792560cdf834ce
kalyani chaudhari <138003080+misskalyani@users.noreply.github.com>
2024-02-11 16:41:47 +0530

    Add files via upload

---
+++ Pattern.c/Square/Lowercase_Square_Combine2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("Enter Limit : ");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		for(j=1;j<=n;j++)
+		{
+			if(i%2==0)
+			{ 
+			   printf("%d ",j);	
+			}
+			else
+			{
+				printf("%c ",(char)(j+96));
+				
+			}
+		}
+		printf("\n");
+	}
+}
